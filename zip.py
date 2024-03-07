4d88f221e536fbfc24f7861565519eb68259649f
Jake Clause <jakeclause@gmail.com>
2024-03-07 11:07:52 -0500

    Added documentation and multiple ID search functionality

--- zip.py
+++ zip.py
@@ -4,20 +4,26 @@ import tempfile
 import zipfile
 
 def main():
-    file_id = getFileIdFromUser()
-    zipFilesWithId(file_id)
+    file_ids = getFileIdsFromUser()
+    zipFilesWithIds(file_ids)
 
-def getFileIdFromUser():
-    return input("Enter the file ID to search for: ")
+# Prompts the user to enter ID's for a desired file. 
+#   (Ex: "IHS-2005-0004, CRB-2009-0003, CRB-2006-0005" or "CRB-2009-0003")
+def getFileIdsFromUser():
+    ids = input("Enter the file IDs separated by commas: ")
+    return [id.strip() for id in ids.split(',')]
 
-def searchFilesWithId(file_id):
+# Based off the ID's given search for files containing them in their file name
+def searchFilesWithIds(file_ids):
     matching_files = []
     for root, dirs, files in os.walk(".", topdown=True):
         for file in files:
-            if file_id in file:
-                matching_files.append(os.path.join(root, file))
+            for file_id in file_ids:
+                if file_id in file:
+                    matching_files.append(os.path.join(root, file))
     return matching_files
 
+# Copy the files found to a temp folder
 def copyFilesToTempDirectory(files):
     temp_dir = tempfile.mkdtemp()
     for file_path in files:
@@ -27,8 +33,10 @@ def copyFilesToTempDirectory(files):
         shutil.copy(file_path, temp_file_path)
     return temp_dir
 
-def createZipFile(temp_dir, file_id):
-    output_zip = os.path.join(os.getcwd(), file_id + '.zip')
+# Creates a zip file with files corresponding to given IDs.
+# Searches for files containing specified IDs in their names and zips them.
+def createZipFile(temp_dir, file_ids):
+    output_zip = os.path.join(os.getcwd(), '_'.join(file_ids) + '.zip')
     with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for root, dirs, files in os.walk(temp_dir):
             for file in files:
@@ -37,17 +45,18 @@ def createZipFile(temp_dir, file_id):
                 zip_file.write(file_path, relative_path)
     return output_zip
 
-def zipFilesWithId(file_id):
-    matching_files = searchFilesWithId(file_id)
+# Searches for files in the current working directory that contain the specified file IDs in their filenames.
+def zipFilesWithIds(file_ids):
+    matching_files = searchFilesWithIds(file_ids)
     if matching_files:
         temp_dir = copyFilesToTempDirectory(matching_files)
         try:
-            output_zip = createZipFile(temp_dir, file_id)
-            print("Files with ID '{}' zipped to {}".format(file_id, output_zip))
+            output_zip = createZipFile(temp_dir, file_ids)
+            print("Files with IDs '{}' zipped to {}".format(', '.join(file_ids), output_zip))
         finally:
             shutil.rmtree(temp_dir)
     else:
-        print("No files found with ID '{}'.".format(file_id))
+        print("No files found with IDs '{}'.".format(', '.join(file_ids)))
 
 if __name__ == "__main__":
     main()
