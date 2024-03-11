65a59c7a33d656cd489e2a9c51c29ae8868b5be7
Thomas Abato <abatot@moravian.edu>
2024-03-11 16:06:54 -0400

    Short term additions to html

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 # app.py
+import os
+import shutil as sh
 from flask import Flask, render_template, request
-from zip import getFileIdsFromUser, searchFilesWithIds, zipSampleData, main
+from zip import download_from_s3, zipSampleData
 
-def get_file_ids_from_user():
-    return [id.strip() for id in ids.split(',')]
+def process_file_ids(file_ids):
+    if os.path.exists("temp-data"):
+        sh.rmtree('temp-data')
+    for file_id in file_ids:
+        print(file_id)
+        download_from_s3(file_id)  # Call functions from the imported zip module
+    zipSampleData()  # Call functions from the imported zip module
+    print("data.zip has been created")
+    sh.rmtree('temp-data')
 
 app = Flask(__name__)
 
@@ -11,15 +20,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['GET'])
-def search():
-    file_ids = request.args.get('file_ids')
-    if file_ids:
-        file_ids = get_file_ids_from_user(file_ids)
-        files = searchFilesWithIds(file_ids)
-        return render_template('search.html', files=files)
-    else:
-        return "No file ids provided"
+# Handling the form submission
+@app.route('/docket-id', methods=['POST'])
+def handle_form_submission():
+    # Get the 'docket-id' from the form data
+    docket_id = request.form['docket-id']
+    
+    # Pass the 'docket-id' to another function
+    result = process_file_ids(docket_id)
+    
+    # Return some response to the client
+    return 'Docket ID: {}'.format(result)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
