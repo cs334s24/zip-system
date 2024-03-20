28c7fd365c4aef96d6d3748c3ebb2a8e3f728875
JeremyGoll <GollJ@moravian.edu>
2024-03-20 13:53:16 -0400

    added more tests

--- test_zip.py
+++ test_zip.py
@@ -1,25 +1,29 @@
-import unittest
-from unittest.mock import patch
-from io import StringIO
-from zip import getFileIdsFromUser, searchFilesWithIds, zipSampleData
+import os
+import zipfile
+import sh
+from zip import zipSampleData, putZipToS3, download_from_s3, getFileIdsFromUser, searchFilesWithIds
 
-class TestScript(unittest.TestCase):
-    @patch('builtins.input', side_effect=['IHS-2005-0004, CRB-2009-0003, CRB-2006-0005'])
-    def test_getFileIdsFromUser(self, mock_input):
-        expected_ids = ['IHS-2005-0004', 'CRB-2009-0003', 'CRB-2006-0005']
-        result = getFileIdsFromUser()
-        self.assertEqual(result, expected_ids)
+def test_zipSampleData():
+   zipSampleData()
+   assert os.path.exists("data.zip")
+   os.remove("data.zip")
 
-    def test_searchFilesWithIds(self):
-        file_ids = ['IHS-2005-0004', 'CRB-2009-0003']
-        expected_files = ['temp-data/data/IHS-2005-0004/somefile.txt']
-        result = searchFilesWithIds(file_ids)
-        self.assertEqual(result, expected_files)
+def test_getFileIdsFromUser():
+   test_input = "IHS-2005-0004, CRB-2009-0003, CRB-2006-0005"
+   expected_output = ["IHS-2005-0004", "CRB-2009-0003", "CRB-2006-0005"]
+   assert getFileIdsFromUser(test_input) == expected_output
 
-    def test_zipSampleData(self):
-        with patch('zipfile.ZipFile') as mock_zipfile:
-            zipSampleData()
-            mock_zipfile.assert_called_once_with('data.zip', 'w')
+def test_searchFilesWithIds():
+   test_file_ids = ["IHS-2005-0004", "CRB-2009-0003"]
+   expected_matching_files = ["./path/to/IHS-2005-0004-file.txt", "./path/to/CRB-2009-0003-file.txt"]
+   assert searchFilesWithIds(test_file_ids) == expected_matching_files
 
-if __name__ == '__main__':
-    unittest.main()
+def test_putZipToS3():
+   putZipToS3("data.zip")
+   assert sh.aws('s3', 'cp', 'data.zip', 's3://zip-system-put') == 0
+
+def test_download_from_s3():
+   test_docket_id = "IHS-2005-0004"
+   download_from_s3(test_docket_id)
+   assert os.path.exists("temp-data/data/IHS-2005-0004")
+   sh.rm('-r', 'temp-data')
